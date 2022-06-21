2b2d8ff6becbdbf50b0adc22d0fbdfa33831274d
vcomm <liskovyakov@gmail.com>
2022-06-21 14:53:23 +0300

    add new methods

--- src/service.deploy.js
+++ src/service.deploy.js
@@ -1,9 +1,41 @@
-const { aChainEngine } = require('@vcomm/asynchain');
+const { aChainEngine, asynChain } = require('@vcomm/asynchain');
 
 class serviceDeploy extends aChainEngine {
     constructor() {
         super();      
     }
+
+    emitted(evname, funclst, cntx, cblk) {
+        this.chains[evname] = new asynChain(funclst);
+        this.chains[evname].seqClone();
+        this.on(evname, (context) => this.chains[evname].queuecall(context || cntx, () => {
+            console.log(`: Finish On Chain[${evname}] and restore: `, this.eventNames());
+            if (cblk) cblk();
+        }, true), cntx);
+        return this.eventNames();
+    }
+
+    waterfall(evname, cntx, cblk) {
+        if (this.chains[evname]) {
+            this.chains[evname].seqClone();
+            this.chains[evname].queuecall(cntx, cblk, true);
+        } else {    
+            console.warn(`: Chain[${evname}] not exist: `, this.eventNames());
+        }
+    }
+
+    async execWaterfall(evname, cntx) {
+        try {
+            await new Promise((resolve) => {
+                this.waterfall(evname, cntx, () => resolve(true))
+            });
+            return;
+        } catch (err) {
+            console.error(`: execWaterfall error: `,err);
+            throw err;
+        }
+    }
+
     deployLogic(logic) {
 
     }
